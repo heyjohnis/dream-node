6f94428316917a9f4d4457eee6a095b9ca155c77
Heujoon Kim <juni0227@nate.com>
2021-09-19 23:31:59 +0900

    11 : buffer

---
+++ 11-buffer/app.js
@@ -0,0 +1,18 @@
+const buf = Buffer.from('Hi');
+console.log(buf);
+console.log(buf.length);
+console.log(buf[0]);
+console.log(buf[1]);
+console.log(buf.toString());
+
+const buf2 = Buffer.alloc(2);
+const buf3 = Buffer.allocUnsafe(2);
+buf2[0] = 72;
+buf2[1] = 105;
+console.log(buf2);
+buf2.copy(buf3)
+console.log(buf2.toString());
+console.log(buf3);
+
+const newBuf = Buffer.concat([buf, buf2, buf3]);
+console.log(newBuf.toString());
