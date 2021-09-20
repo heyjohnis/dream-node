2c42615efc5990be5e7590a2e5780ea55a1be651
Heujoon Kim <juni0227@nate.com>
2021-09-20 12:39:40 +0900

    npm : 명령어

---
+++ npm/app.js
@@ -0,0 +1,5 @@
+// npm info <module>
+// npm outdated <module>  # updated 대상 module
+// npm ll -g --depth=0 # ll : List, --depth : 조회범주
+// npm view <module> # view : module 정보
+// npm un <module> # un -> uninstall
