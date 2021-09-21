3133cc66363d5ef087b928ff467a2768f81e6d37
Heujoon Kim <juni0227@nate.com>
2021-09-21 14:50:10 +0900

    ejs 서버예시

---
+++ node-server/2-template.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const ejs = require('ejs');
+
+const name = 'John';
+const course = [
+  {name: 'HTML'},
+  {name: 'CSS'},
+  {name: 'JS'},
+]
+
+const server = http.createServer((req, res) => {
+  console.log('incoming...');
+  console.log('httpVersion: ', req.httpVersion);
+  console.log('headers: ', req.headers);
+  console.log('method: ', req.method);
+  console.log('url: ', req.url);
+
+  const url = req.url;
+  res.setHeader('Content-Type', 'text/html');
+  if(url === '/') {
+    ejs.renderFile('./template/index.ejs', { name })
+      .then(data => res.end(data));
+  } else if(url === '/course') {
+    ejs.renderFile('./template/course.ejs', { course })
+      .then(data => res.end(data));
+  } else {
+    ejs.renderFile('./template/not-found.ejs', { name })
+      .then(data => res.end(data));
+  }
+});
+
+server.listen(8080);
