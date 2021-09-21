c83b08f60a1393a5f6554b9606b90a1cccf1733d
Heujoon Kim <juni0227@nate.com>
2021-09-21 15:26:58 +0900

    json 보여주기

---
+++ node-server/3-json.js
@@ -0,0 +1,34 @@
+const http = require('http');
+
+const courses = [
+  {name: 'HTML'},
+  {name: 'CSS'},
+  {name: 'JS'},
+]
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if(url === '/course') {
+    if(method === 'GET') {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(courses));
+    } else if(method === 'POST') {
+      const body = [];
+      req.on('data',(chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+      });
+      req.on('end', () => {
+        const course = JSON.parse(Buffer.concat(body).toString());
+        courses.push(course);
+        console.log(course);
+        res.writeHead(201);
+        res.end();
+      });
+
+    }
+  }
+});
+
+server.listen(8080);
