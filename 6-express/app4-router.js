711de41e082f3904c5fd2704ef1c64a809be36b7
Heujoon Kim <juni0227@nate.com>
2021-09-22 10:11:22 +0900

    express router

--- 6-express/app4-router.js
+++ 6-express/app4-router.js
@@ -1,56 +1,12 @@
 import express from 'express';
+import postRouter from './router/post.js';
+import userRouter from './router/user.js';
+
 const app = express();
 
-// app.get('/sky/:id', (req, res, next) => {
-//   console.log('get');
-//   console.log(req.path);
-//   console.log(req.headers);
-//   console.log(req.params);
-//   console.log(req.query);
-//   res.setHeader('key', 'value');
-//   //res.send('hi');
-//   res.status(201).send('created');
-// });
+app.use(express.json());
 
-app.all('/api', (req, res, next) => {
-  console.log('all');
-  next();
-});
-
-app.get('/', 
-  (req, res, next) => {
-    console.log('first');
-    next();
-  },
-  (req, res, next) => {
-    console.log('second');
-    next();
-  }
-);
-
-app.get(
-  '/', 
-  (req, res, next) => {
-    console.log('third');
-    res.send('complete');
-  }
-);
-
-app.use(express.json())
-
-app.post('/post', (req, res, next) => {
-  console.log(req.body);
-});
-
-app.use((req, res, next) => {
-  res.status(404).send('Not available!');
-});
-
-app.use((error, req, res, next) => {
-  console.error(error);
-  res.status(500).send('Sorry, try later!');
-});
-
-
+app.use('/posts', postRouter);
+app.use('/users', userRouter);
 
 app.listen(8080);
