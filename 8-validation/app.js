9a509b6c84af5e5a0db656303882580465a79f61
heyjohnis <juni0227@nate.com>
2021-11-21 17:05:37 +0900

    14-validation: refactoring

--- 8-validation/app.js
+++ 8-validation/app.js
@@ -4,18 +4,25 @@ import { body, param, validationResult } from 'express-validator';
 const app = express();
 app.use(express.json());
 
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if(errors.isEmpty()) {
+    return next();
+  } else {
+    return res.status(400).json({message: errors.array()})
+  }
+}
+
 app.post('/user', 
   [
     body('name').isLength({min:2, max:10}).withMessage('이름은 두글짜 이상'),
     body('age').isInt().withMessage('숫자를 입력하세요'),
     body('email').isEmail().withMessage('email을 입력해'),
-    body('job.name').notEmpty().withMessage('직업명을 입력하세요')
+    body('job.name').notEmpty().withMessage('직업명을 입력하세요'),
+    validate
   ],
   (req, res, next) => {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()) {
-    return res.status(400).json({message: errors.array()});
-  }
+
   console.log(req.body);
 
   res.sendStatus(201);
@@ -25,12 +32,10 @@ app.get(
   '/:email', 
   [
     param('email').isEmail().withMessage('이메일을 입력하세요'),
+    validate
   ],
   (req, res, next) => {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()) {
-    return res.status(400).json({message: errors.array()});
-  }
+
   console.log(req.body);
   res.send('🐼')
 });
