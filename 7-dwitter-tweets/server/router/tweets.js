250b3bdf8940d0fb2af153e45e1ee4a4cefd9e6c
heyjohnis <juni0227@nate.com>
2021-11-21 18:46:58 +0900

    14-validation: dwitter Validation 적용

--- 7-dwitter-tweets/server/router/tweets.js
+++ 7-dwitter-tweets/server/router/tweets.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import 'express-async-errors';
-import * as tweetController from '../controller/tweet.js'
+import { body } from 'express-validator';
+import * as tweetController from '../controller/tweet.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+
+const validateTweet = [
+  body('text').trim().isLength({min:3}).withMessage('최소 3글짜 이상 입력'),
+  validate
+];
 
 // GET /tweets
 // GET /tweets?username=:username
@@ -12,10 +19,10 @@ router.get('/', tweetController.getTweets);
 router.get('/:id', tweetController.getTweet);
 
 // POST /tweets
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 // DELET /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
